76f6d7122cf3719b3442cb8229f4159d93db5488
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2024-07-30 21:22:09 +0300

    binary search

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-// 1-linearsearch
-const arr = [1, 4, 5, 6, 8, 5, 1, 2, 7, 5, 2, 11];
+// 1. Linear search
+// const arr = [1, 4, 5, 6, 8, 5, 1, 2, 7, 5, 2, 11];
+// let count = 0;
+// function linearSearch(arr, item) {
+//   for (let i = 0; i < arr.length; i++) {
+//     count += 1;
+//     if (arr[i] === item) {
+//       return i;
+//     }
+//   }
+//   return null;
+// }
+// console.log(linearSearch(arr, 8));
+// console.log(`count = ${count}`);
+// 2. Binary search
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let count = 0;
-function linearSearch(arr, item) {
-  for (let i = 0; i < arr.length; i++) {
-    count += 1;
-    if (arr[i] === item) {
-      return i;
+function binarySearch(arr, item) {
+  count += 1;
+  let start = 0;
+  let end = arr.length;
+  let middle;
+  let found = false;
+  let position = -1;
+  while (found === false && start <= end) {
+    middle = Math.floor((start + end) / 2);
+    if (arr[middle] === item) {
+      found = true;
+      position = middle;
+      return position;
+    }
+    if (item < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
     }
   }
-  return null;
+  return position;
 }
-console.log(linearSearch(arr, 8));
-console.log(`count = ${count}`);
+console.log(binarySearch(array, 1));
+console.log(binarySearch(array, 8));
+console.log(binarySearch(array, 99));
+console.log(count);
