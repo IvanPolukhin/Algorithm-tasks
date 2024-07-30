ebb4f3e1a04e902a000e1ad9c92b4100c042f5c0
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2024-07-30 20:06:51 +0300

    linear search

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1-linearsearch
+const arr = [1, 4, 5, 6, 8, 5, 1, 2, 7, 5, 2, 11];
+let count = 0;
+function linearSearch(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    count += 1;
+    if (arr[i] === item) {
+      return i;
+    }
+  }
+  return null;
+}
+console.log(linearSearch(arr, 8));
+console.log(`count = ${count}`);
