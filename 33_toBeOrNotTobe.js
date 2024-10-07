f99f40d8ac905134587784e027dac262518c8c72
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2024-10-07 20:16:52 +0300

    add value comparison using toBe and notToBe methods

---
+++ 33_toBeOrNotTobe.js
@@ -0,0 +1,21 @@
+function expect(val) {
+  return {
+    toBe: function (expected) {
+      if (val === expected) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: function (expected) {
+      if (val !== expected) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
+}
+
+console.log(expect(5).toBe(5));
+console.log(expect(2).toBe(5));
