28380ad054e635b0b8a99f00a154203ceb9ada49
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2024-12-05 21:56:44 +0200

    using for...of and reduce in sumArray

--- 20_sum_arr.js
+++ 20_sum_arr.js
@@ -1,9 +1,24 @@
+// use for
 let array = [1, 2, 3, 4, 5];
 let sumArray = (arr) => {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
+  return sum;
+};
+
+// use reduce
+const sumArrayWithReduce = (arr) => arr.reduce((acc, val) => acc + val, 0);
+
+// use for...of
+const sumArrayForOf = (arr) => {
+  let sum = 0;
+
+  for (let num of arr) {
+    sum += num;
+  }
+
   return sum;
 };
 
