142d85a5076f405c369f0105b5cd5bd08def34a7
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2024-12-19 17:58:10 +0200

    fix: findSecondLargest

--- 72_findSecondLargest.js
+++ 72_findSecondLargest.js
@@ -1,9 +1,14 @@
 const findSecondLargest = (arr) => {
-  if (arr.length === 2) return null;
+  if (arr.length < 2) return null;
 
   const uniqueSorted = [...new Set(arr)].sort((a, b) => a - b);
+
+  if (uniqueSorted.length < 2) return null;
 
   return uniqueSorted[uniqueSorted.length - 2];
 };
 
 console.log(findSecondLargest([10, 20, 30, 40]));
+console.log(findSecondLargest([5, 5, 5, 5]));
+console.log(findSecondLargest([1]));
+console.log(findSecondLargest([3, 2, 1]));
