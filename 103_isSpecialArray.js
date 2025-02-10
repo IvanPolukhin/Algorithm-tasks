c32ff0a628d0819a38a6e2a4b8c3147fb9c2e8ec
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2025-02-10 19:02:12 +0200

    add: fnc isSpecialArray second solution

--- 103_isSpecialArray.js
+++ 103_isSpecialArray.js
@@ -1,8 +1,25 @@
-const isSpecialArray = (arr) => {
+const isSpecialArrayV1 = (arr) => {
   return arr.slice(1).every((el, i) => arr[i] % 2 !== el % 2);
 };
 
-console.log(isSpecialArray([1, 2, 3, 4]));
-console.log(isSpecialArray([2, 4, 6, 8]));
-console.log(isSpecialArray([1, 3, 5, 7]));
-console.log(isSpecialArray([7, 6, 3, 8, 1, 2]));
+console.log(isSpecialArrayV1([1, 2, 3, 4]));
+console.log(isSpecialArrayV1([2, 4, 6, 8]));
+console.log(isSpecialArrayV1([1, 3, 5, 7]));
+console.log(isSpecialArrayV1([7, 6, 3, 8, 1, 2]));
+
+// second solution
+
+const isSpecialArrayV2 = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if ((arr[i] + arr[i + 1]) % 2 === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(isSpecialArrayV2([1, 2, 3, 4]));
+console.log(isSpecialArrayV2([2, 4, 6, 8]));
+console.log(isSpecialArrayV2([1, 3, 5, 7]));
+console.log(isSpecialArrayV2([7, 6, 3, 8, 1, 2]));
