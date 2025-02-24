36279fd7689bd99a749dc05db93c14e2b0f35ad5
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2025-02-24 19:21:50 +0200

    add: fnc romanToInt

---
+++ 114_romanToInt.js
@@ -0,0 +1,25 @@
+const romanToInt = (s) => {
+    const romanMap = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+
+    let total = 0;
+    let prevValue = 0;
+
+    for (let char of s) {
+        const currentValue = romanMap[char];
+
+        if (currentValue > prevValue) {
+            total += currentValue - 2 * prevValue;
+        } else {
+            total += currentValue;
+        }
+
+        prevValue = currentValue;
+    }
+
+    return total;
+};
+
+console.log(romanToInt("VIII"));
+console.log(romanToInt("IX"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
