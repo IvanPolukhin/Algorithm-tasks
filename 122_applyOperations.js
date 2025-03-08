348e24f8500203dc6c068837a7876a0d67d30b4f
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2025-03-08 21:52:23 +0200

    add: fnc applyOperations

---
+++ 122_applyOperations.js
@@ -0,0 +1,18 @@
+const applyOperations = (nums) => {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        }
+    }
+
+    const result = nums.filter((num) => num !== 0);
+
+    while (result.length < nums.length) {
+        result.push(0);
+    }
+
+    return result;
+};
+
+console.log(applyOperations([1, 2, 2, 1, 1, 0]));
