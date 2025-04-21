3bb6bc5ebaaf7ced1ec9a9a3d71174963b3d767a
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2025-04-21 20:06:52 +0300

    feat: add fnc sortedWords

---
+++ 150_sortStringsByLength.js
@@ -0,0 +1,7 @@
+const words = ["apple", "banana", "kiwi", "grapefruit", "pear"];
+
+const sortedWords = (words) => {
+    return words.sort((a, b) => a.length - b.length);
+}
+
+console.log(sortedWords(words));
