1cb3f683c30a797d4d62fe8e4d60469b6b9b3844
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2025-04-21 20:07:23 +0300

    refactor

--- 150_sortStringsByLength.js
+++ 150_sortStringsByLength.js
@@ -2,6 +2,6 @@ const words = ["apple", "banana", "kiwi", "grapefruit", "pear"];
 
 const sortedWords = (words) => {
     return words.sort((a, b) => a.length - b.length);
-}
+};
 
-console.log(sortedWords(words));
+console.log(sortedWords(words)); // [ 'kiwi', 'pear', 'apple', 'banana', 'grapefruit' ]
