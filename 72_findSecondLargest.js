380ff5d58ffc8f3a14ad150e0cfa5356d48cd847
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2025-05-14 10:48:11 +0300

    feat: add fnc findSecondLargest2

--- 72_findSecondLargest.js
+++ 72_findSecondLargest.js
@@ -1,14 +1,41 @@
 const findSecondLargest = (arr) => {
-  if (arr.length < 2) return null;
+    if (arr.length < 2) return null;
 
-  const uniqueSorted = [...new Set(arr)].sort((a, b) => a - b);
+    const uniqueSorted = [...new Set(arr)].sort((a, b) => a - b);
 
-  if (uniqueSorted.length < 2) return null;
+    if (uniqueSorted.length < 2) return null;
 
-  return uniqueSorted[uniqueSorted.length - 2];
+    return uniqueSorted[uniqueSorted.length - 2];
+};
+
+// Alternative
+
+const findSecondLargest2 = (arr) => {
+    const unique = [...new Set(arr)];
+
+    if (unique.length < 2) return null;
+
+    let max = -Infinity;
+    let second = -Infinity;
+
+    for (let num of unique) {
+        if (num > max) {
+            second = max;
+            max = num;
+        } else if (num > second) {
+            second = num;
+        }
+    }
+
+    return second;
 };
 
 console.log(findSecondLargest([10, 20, 30, 40]));
 console.log(findSecondLargest([5, 5, 5, 5]));
 console.log(findSecondLargest([1]));
 console.log(findSecondLargest([3, 2, 1]));
+
+console.log(findSecondLargest2([10, 20, 30, 40]));
+console.log(findSecondLargest2([5, 5, 5, 5]));
+console.log(findSecondLargest2([1]));
+console.log(findSecondLargest2([3, 2, 1]));
